76a78590d4348487c9891b890b1b504c0b5a8098
Isaac Alexis Rivera Sanchez <ariveras@uni.pe>
2025-08-27 01:24:48 -0500

    s

--- riesgosutils/s3.py
+++ riesgosutils/s3.py
@@ -240,7 +240,7 @@ class S3ConnectionMR:
     def s3_upload_df(self, df, key, config=None, engine='csv') -> None:
         try:
             # Determinar extensión
-            file_extension = filename.split(".")[-1].lower()
+            file_extension = key.split(".")[-1].lower()
             suffix = '.parquet' if file_extension == 'parquet' else '.csv'
     
             # Crear archivo temporal
@@ -479,6 +479,7 @@ class DynamoDBConnection:
         except ClientError as e:
             logging.error(f"Error querying table {self.table.name}: {str(e)}")
             return []
+
 
 
 
